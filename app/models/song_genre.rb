f863d3e3c7b9dfcdf28963417bedfa80c4504ea0
Keenah Vancampenhout <kvancamp@mail.com>
2021-02-26 20:17:29 +0000

    Automatically backed up by Learn

--- app/models/song_genre.rb
+++ app/models/song_genre.rb
@@ -1,7 +1,7 @@
 require_relative './concerns/slugifiable.rb'
 
-class SongGenre < ActiveRecord::Base 
-    belongs_to :song 
+class SongGenre < ActiveRecord::Base
+    belongs_to :song
     belongs_to :genre
 
     include Slugifiable::InstanceMethod
