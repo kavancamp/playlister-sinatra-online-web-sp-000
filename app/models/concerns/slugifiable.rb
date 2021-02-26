3dc5cc462e742a725fda24395b0d5c5a0d49c219
Keenah Vancampenhout <kvancamp@mail.com>
2021-02-26 20:34:32 +0000

    Automatically backed up by Learn

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -1,26 +1,15 @@
-module Slugifiable
-  module InstanceMethods
-    def slug
-      artist_name = self.name
-      slug = artist_name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
-    end
-  end
-
-  module ClassMethods
-    def find_by_slug(slug)
-      @slug = slug
-      format_slug_beginning
-      results = self.where("name LIKE ?", @short_slug)
-      results.detect do |result|
-        result.slug === @slug
-      end
+module Slugifiable 
+    module InstanceMethod 
+        def slug 
+            string = self.name 
+            string.parameterize
+        end
     end
 
-    def format_slug_beginning
-      slug_beginning = @slug.split("-")[0]
-      slug_beginning.prepend("%")
-      slug_beginning << "%"
-      @short_slug = slug_beginning
+    module ClassMethod 
+        def find_by_slug(slug)
+            self.all.find {|string| string.slug == slug} 
+        end
     end
-  end
+
 end
